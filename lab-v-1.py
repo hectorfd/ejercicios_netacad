5eb8f3eb75459e0ad43a54b6725ce3e9fb654529
Hector <61565698+hectorfd@users.noreply.github.com>
2025-03-18 13:09:31 -0500

    lab-v-1 terminado

---
+++ lab-v-1.py
@@ -0,0 +1,7 @@
+# Usando uno de los operadores de comparación en Python, escribe un programa simple de dos líneas
+# que tome el parámetro n como entrada, que es un entero, e imprime False si n es menor que 100, 
+# y True if n es mayor o igual que 100.
+
+n = int(input('Ingrese un número entero: '))
+print('True' if n>=100 else 'False')  
+
