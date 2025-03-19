28108538a01f10f77dabc25970b951e7e5abaf4c
Hector <61565698+hectorfd@users.noreply.github.com>
2025-03-19 11:43:55 -0500

    Espatifilo

---
+++ lab-oe03.py
@@ -0,0 +1,8 @@
+entrada = input('introduce un texto\n')
+
+if entrada == 'Espatifilo': 
+    print('Sí, el Espatifilo es la mejor planta de interior')
+elif entrada == 'espatifilo': 
+    print('No, quiero un gran Espatifilo')
+else:
+    print('Espatifilo! No', entrada, '!')
